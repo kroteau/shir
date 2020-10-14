79ba5955abea5a015dfbe91e6dfe70c06208149b
Vladimir Lazarenko <favoretti@gmail.com>
2020-10-14 10:55:45 +0200

    Fix registration

--- install-gateway.ps1
+++ install-gateway.ps1
@@ -19,9 +19,10 @@ function Install-Gateway([string] $gwPath)
 function Register-Gateway([string] $key)
 {
     Write-Host "Start to register gateway with key: $key"
-    Start-Process "C:\Program Files\Microsoft Integration Runtime\4.0\Shared\dmgcmd.exe" -RegisterNewNode "$key" "$env:COMPUTERNAME"
-    Start-Process "C:\Program Files\Microsoft Integration Runtime\4.0\Shared\dmgcmd.exe" -EnableRemoteAccess 8060
+    & "C:\Program Files\Microsoft Integration Runtime\4.0\Shared\dmgcmd.exe" @("-RegisterNewNode", "$key", "$env:COMPUTERNAME")
+    & "C:\Program Files\Microsoft Integration Runtime\4.0\Shared\dmgcmd.exe" @("-EnableRemoteAccess", "8060")
     Write-Host "Succeed to register gateway"
+}
 
 }
 
