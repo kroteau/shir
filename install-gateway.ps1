b9881ff822054885a541a7952d669706181a5d4d
Yoram Hekma <yoram@ops-dev.nl>
2022-01-06 12:18:57 +0100

    fix indents

--- install-gateway.ps1
+++ install-gateway.ps1
@@ -8,37 +8,37 @@ function Install-JRE([string] $source)
 {
 # Download and silent install Java Runtime Environement
 
-# working directory path  
+# working directory path
   $workd = "c:\temp"
 
-# Check if work directory exists if not create it  
-    If (!(Test-Path -Path $workd -PathType Container))  
-    {   
-	    New-Item -Path $workd  -ItemType directory   
+# Check if work directory exists if not create it
+    If (!(Test-Path -Path $workd -PathType Container))
+    {
+      New-Item -Path $workd -ItemType directory
     }
 
-#create config file for silent install  
-  $text = '  
-    INSTALL_SILENT=Enable  
-    AUTO_UPDATE=Enable  
-    SPONSORS=Disable  
-    REMOVEOUTOFDATEJRES=1  
-    '  
-    $text | Set-Content "$workd\jreinstall.cfg"  
-          
-#download executable, this is the small online installer  
-    $source = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245477_4d5417147a92418ea8b615e228bb6935"  
-    $destination = "$workd\jreInstall.exe"  
-    $client = New-Object System.Net.WebClient  
+#create config file for silent install
+  $text = '
+    INSTALL_SILENT=Enable
+    AUTO_UPDATE=Enable
+    SPONSORS=Disable
+    REMOVEOUTOFDATEJRES=1
+    '
+    $text | Set-Content "$workd\jreinstall.cfg"
+
+#download executable, this is the small online installer
+    $source = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245477_4d5417147a92418ea8b615e228bb6935"
+    $destination = "$workd\jreInstall.exe"
+    $client = New-Object System.Net.WebClient
     $client.DownloadFile($source, $destination)
 
-#install silently  
+#install silently
     Start-Process -FilePath "$workd\jreInstall.exe" -ArgumentList INSTALLCFG="$workd\jreinstall.cfg"
 
-# Wait 120 Seconds for the installation to finish  
+# Wait 120 Seconds for the installation to finish
     Start-Sleep -s 180
 
-# Remove the installer  
+# Remove the installer
     rm -Force $workd\jre*
 }
 
