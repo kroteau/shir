840fa7c5ba2033a678aebf5cedddc358fe960ec0
Stas Krotov <105755981+kroteau@users.noreply.github.com>
2023-05-24 03:56:09 +0300

    Moved validation logic to script parameters; moved all download handing into a separate function; Added verbose output; Cleanup; Args are backwards compatible

--- install-gateway.ps1
+++ install-gateway.ps1
@@ -1,79 +1,144 @@
 #### Here is the usage doc:
-#### PS D:\GitHub> .\InstallGatewayOnLocalMachine.ps1 E:\shared\bugbash\IntegrationRuntime.msi <key>
+#### PS D:\GitHub> .\InstallGatewayOnLocalMachine.ps1 E:\shared\bugbash\IntegrationRuntime.msi <key> # defined version mode (backwards compatibility)
+#### PS D:\GitHub> .\InstallGatewayOnLocalMachine.ps1 <key> # latest version mode
+#### PS D:\GitHub> .\InstallGatewayOnLocalMachine.ps1 -authKey <key> -path E:\shared\bugbash\IntegrationRuntime.msi # named parameters (non-ordered)
 ####
+[CmdletBinding(DefaultParameterSetName = 'LatestVersion')]
 
-param([string]$path, [string]$authKey, [string]$jresource = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245479_4d5417147a92418ea8b615e228bb6935") # JRE 8u311
+param(
+  [Parameter(ParameterSetName = 'DefinedVersion', Position = 0, Mandatory)]
+  [ValidateScript({ Test-Path $_ -PathType Leaf })]
+  [string]$path,
 
-function Install-JRE([string] $source)
+  [Parameter(ParameterSetName = 'DefinedVersion', Position = 1, Mandatory)]
+  [Parameter(ParameterSetName = 'LatestVersion', Position = 0, Mandatory)]
+  [ValidateNotNullOrEmpty()]
+  [string]$authKey,
+
+  [string]$jresource = 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=245479_4d5417147a92418ea8b615e228bb6935', # JRE 8u311
+
+  [string]$irsource = 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=39717', # Integration Runtime Download Page
+  [string]$irsource_filter = 'https://download.microsoft.com*msi',
+
+  [string]$vcrsource = 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=26999', # Microsoft Visual C++ 2010 Redistributable (Service Pack 1)
+  [string]$vcrsource_filter = 'https://download.microsoft.com*vcredist_x64.exe',
+  # original url # 'https://download.microsoft.com/download/3/2/2/3224B87F-CFA0-4E70-BDA3-3DE650EFEBA5/vcredist_x64.exe'
+  
+  [string]$workd = 'c:\temp' # working directory path
+)
+
+function Download-File([string]$uri, [string]$workd=$script:workd, [string]$msdl_filter, [string]$outFile)
+{
+  # if the download is a microsoft download page - parse it for file permalinks
+  if (-not [string]::IsNullOrEmpty($msdl_filter))
+  {
+    Write-Verbose "Searching for `"$msdl_filter`" on download page $uri"
+    $page = Invoke-WebRequest -Uri $uri -UseBasicParsing
+    $uri = $page.Links.Where({$_.href -like $msdl_filter}).href | Select-Object -Unique -First 1
+  }
+  
+  # Setting output filename 
+  if (!$outFile)
+  {
+    $outFile = [System.Net.WebUtility]::UrlDecode($uri.Split('/')[-1])
+  }
+
+  $filename = "$workd\$outFile"
+  
+  write-Verbose "Downloading $uri into $filename"
+  (New-Object System.Net.WebClient).downloadFile($uri,$filename)
+
+  Print-FileInfo $filename
+
+  return $filename
+}
+
+function Install-JRE([string]$jresource = $script:jresource, [string]$workd = $script:workd)
 {
 # Download and silent install Java Runtime Environement
-
-# working directory path
-  $workd = "c:\temp"
-
-# Check if work directory exists if not create it
-    If (!(Test-Path -Path $workd -PathType Container))
-    {
-      New-Item -Path $workd -ItemType directory
-    }
-
-#create config file for silent install
+# create config file for silent install
   $text = '
     INSTALL_SILENT=Enable
     AUTO_UPDATE=Enable
     SPONSORS=Disable
     REMOVEOUTOFDATEJRES=1
     '
-    $text | Set-Content "$workd\jreinstall.cfg"
+  $text | Set-Content "$workd\jreinstall.cfg"
 
-#download executable, this is the small online installer
-    $source = $jresource
-    $destination = "$workd\jreInstall.exe"
-    $client = New-Object System.Net.WebClient
-    $client.DownloadFile($source, $destination)
+# download executable, this is the small online installer
+  $filename = Download-File -uri $jresource -outFile 'jreInstall.exe'
 
-#install silently
-    Start-Process -FilePath "$workd\jreInstall.exe" -ArgumentList INSTALLCFG="$workd\jreinstall.cfg"
+# install silently
+  write-Verbose "Install JRE from $filename"
+  $process = Start-Process -FilePath "$filename" -ArgumentList "INSTALLCFG=`"$workd\jreinstall.cfg`"" -Wait -PassThru
 
-# Wait 120 Seconds for the installation to finish
-    Start-Sleep -s 180
+# Wait for the installation to finish
+  Write-Host 'Waiting for jreinstall.exe to finish...'
+  while ((Get-Process jreinstall -ErrorAction SilentlyContinue))
+  {
+    Start-Sleep -Seconds 10
+  }
 
 # Remove the installer
-    rm -Force $workd\jre*
+  rm -Force $workd\jre*
+
+  Print-ExecInfo $process
 }
 
-function Install-VcRedist()
+function Install-VcRedist([string]$vcrsource = $script:vcrsource, [string]$vcrsource_filter = $script:vcrsource_filter)
 {
-  $workd = "c:\temp"
-    If (!(Test-Path -Path $workd -PathType Container))
-    {
-      New-Item -Path $workd -ItemType directory
-    }
-  $destination = "$workd\vcredist_x64.exe"
-    $client = New-Object System.Net.WebClient
-    $client.DownloadFile("https://download.microsoft.com/download/3/2/2/3224B87F-CFA0-4E70-BDA3-3DE650EFEBA5/vcredist_x64.exe", $destination)
-    Start-Process -FilePath "$workd\vcredist_x64.exe" -ArgumentList "/Q" -Wait
+  $filename = Download-File -uri $vcrsource -msdl_filter $vcrsource_filter
+
+  Write-Verbose "Installing VcRedist from $filename"
+  $process = Start-Process -FilePath "$filename" -ArgumentList "/Q /log $workd\vcredist.log" -Wait -PassThru
+  
+  Start-Sleep 2
+  Print-ExecInfo $process
 }
 
-function Install-Gateway([string] $gwPath)
+function Install-Gateway([string]$filename = $script:path, [string]$workd = $script:workd)
 {
 # uninstall any existing gateway
   UnInstall-Gateway
 
-    Write-Host "Start Gateway installation"
+  Write-Host 'Start Gateway installation'
+  
+  Write-Verbose "Installing Gateway from $filename"
+  
+  $process = Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $filename /quiet /passive /L*v $workd\ir.log" -Wait -PassThru
+  Start-Sleep -Seconds 30
 
-    Start-Process "msiexec.exe" "/i $path /quiet /passive" -Wait
-    Start-Sleep -Seconds 30
+  Write-Host "Gateway installer finished with exitCode: $($process.exitcode))"
 
-    Write-Host "Succeed to install gateway"
+  Print-ExecInfo $process
 }
 
 function Register-Gateway([string] $key)
 {
   Write-Host "Start to register gateway with key: $key"
-    & "C:\Program Files\Microsoft Integration Runtime\5.0\Shared\dmgcmd.exe" @("-EnableRemoteAccess", "8060")
-    & "C:\Program Files\Microsoft Integration Runtime\5.0\Shared\dmgcmd.exe" @("-RegisterNewNode", "$key", "$env:COMPUTERNAME")
-    Write-Host "Succeed to register gateway"
+  $executable = "C:\Program Files\Microsoft Integration Runtime\5.0\Shared\dmgcmd.exe"
+  
+  if (test-path $executable)
+  {
+    $p1 = Start-Process -FilePath $executable -ArgumentList @("-EnableRemoteAccess", "8060") -Wait -PassThru
+    $p2 = Start-Process -FilePath $executable -ArgumentList @("-RegisterNewNode", "$key", "$env:COMPUTERNAME") -Wait -PassThru
+  }
+
+  switch($p1.exitCode + $p2.exitCode)
+  {
+    0
+      {
+        Write-Host "Succeed to register the gateway"
+      }
+    $null
+      {
+        Write-Host "Register gateway impossible, executable not found"
+      }
+    default
+      {
+        Write-Host "Gateway registration exit codes $($p1.exitCode)] [$($p2.exitCode)]"
+      }
+  }
 }
 
 function Check-WhetherGatewayInstalled([string]$name)
@@ -91,30 +156,44 @@ function Check-WhetherGatewayInstalled([string]$name)
   return $false
 }
 
-
 function UnInstall-Gateway()
 {
-  [void](Get-WmiObject -Class Win32_Product -Filter "Name='Microsoft Integration Runtime Preview' or Name='Microsoft Integration Runtime'" -ComputerName $env:COMPUTERNAME).Uninstall()
+  Write-Verbose "Calling gwmi.Uninstall() for `"Microsoft Integration Runtime*`" products"
+  [void](Get-WmiObject -Class Win32_Product -Filter "Name LIKE 'Microsoft Integration Runtime%'" -ComputerName .).Uninstall()
     Write-Host "Microsoft Integration Runtime has been uninstalled from this machine."
 }
 
-function Validate-Input([string]$path, [string]$key)
+function Print-FileInfo($filename)
 {
-  if ([string]::IsNullOrEmpty($path))
-  {
-    throw "Gateway path is not specified"
-  }
-
-  if (!(Test-Path -Path $path))
-  {
-    throw "Invalid gateway path: $path"
-  }
-
-  if ([string]::IsNullOrEmpty($key))
-  {
-    throw "Gateway Auth key is empty"
-  }
+  # get file versionInfo and 
+  $info = Get-Item $filename |
+    Select-Object -Expand VersionInfo |
+    Select-Object FileName, FileVersion, ProductVersion |
+    ForEach-Object { $_.psobject.properties } |
+    ForEach-Object { "$($_.Name)=$($_.Value)" }
+  
+  #output a single line for better visibility
+  Write-Host "$($info -join '; ')"
 }
+
+function Print-ExecInfo($processinfo)
+{
+  # prepare process object
+  $process = $processinfo |
+    Select-Object exitCode, startTime, exitTime |
+    ForEach-Object { $_.psobject.properties } |
+    ForEach-Object { "$($_.Name)=$($_.Value)" }
+
+  #output a single line for better visibility
+  Write-Host "$($process -join '; ')"
+}
+
+# Script starting parameters (including default)
+$PsBoundParameters.path = $path
+$PsBoundParameters.workd = $workd
+$PsBoundParameters.authKey = $authKey
+$PsBoundParameters.parameterSetName = $PSCmdlet.ParameterSetName
+Write-Verbose "Parameters:$($PsBoundParameters | Out-String)"
 
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
       [Security.Principal.WindowsBuiltInRole] "Administrator"))
@@ -123,9 +202,18 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Break
 }
 
-Validate-Input $path $authKey
+# Check if work directory exists if not create it
+If (!(Test-Path -Path $workd -PathType Container))
+{
+  New-Item -Path $workd -ItemType directory
+}
 
 Install-VcRedist
-Install-JRE $jresource
+Install-JRE
+if ($PSCmdlet.ParameterSetName -eq 'LatestVersion')
+{
+  # if path does not exist or not set falling back to IR remote install / latest version
+  $path = Download-File -uri $irsource -msdl_filter $irsource_filter
+}
 Install-Gateway $path
 Register-Gateway $authKey
