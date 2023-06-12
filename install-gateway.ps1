e1a49ce2e4ad70ac682a9ec42e643d9d9c726ff3
Stas Krotov <105755981+kroteau@users.noreply.github.com>
2023-06-13 00:53:43 +0300

    Proper powershell code style (as it was breaking one of foreach-object ocassionally); Cleanup for old uninstall function; Proxy function for Start-Process; Merge *info-functions into a single one

--- install-gateway.ps1
+++ install-gateway.ps1
@@ -31,34 +31,32 @@ param(
   [string]$workd = 'c:\temp' # working directory path
 )
 
-function Download-File([string]$uri, [string]$workd=$script:workd, [string]$msdl_filter, [string]$outFile)
-{
+function Download-File([string]$uri, [string]$workd=$script:workd, [string]$msdl_filter, [string]$outFile) {
   # if the download is a microsoft download page - parse it for file permalinks
-  if (-not [string]::IsNullOrEmpty($msdl_filter))
-  {
+  if (-not [string]::IsNullOrEmpty($msdl_filter)) {
     Write-Verbose "Searching for `"$msdl_filter`" on download page $uri"
     $page = Invoke-WebRequest -Uri $uri -UseBasicParsing
     $uri = $page.Links.Where({$_.href -like $msdl_filter}).href | Select-Object -Unique -First 1
   }
   
   # Setting output filename 
-  if (!$outFile)
-  {
+  if (!$outFile) {
     $outFile = [System.Net.WebUtility]::UrlDecode($uri.Split('/')[-1])
   }
 
+  # we'd need full path further
   $filename = "$workd\$outFile"
   
-  write-Verbose "Downloading $uri into $filename"
+  Write-Verbose "Downloading $uri into $filename"
   (New-Object System.Net.WebClient).downloadFile($uri,$filename)
 
-  Print-FileInfo $filename
+  # unfortunately does not work for MSI packages, only for EXE
+  Print-ObjectInfo -psObject ((Get-Item $filename).VersionInfo) -propertyNames @('FileName','FileVersion','ProductVersion')
 
   return $filename
 }
 
-function Install-JRE([string]$jresource = $script:jresource, [string]$workd = $script:workd, [int]$jreinstall_timeout = $script:jreinstall_timeout)
-{
+function Install-JRE([string]$jresource = $script:jresource, [string]$workd = $script:workd, [int]$jreinstall_timeout = $script:jreinstall_timeout) {
 # Download and silent install Java Runtime Environement
 # create config file for silent install
   $text = '
@@ -73,189 +71,142 @@ function Install-JRE([string]$jresource = $script:jresource, [string]$workd = $s
   $filename = Download-File -uri $jresource -outFile 'jreInstall.exe'
 
 # install silently
-  write-Verbose "Install JRE from $filename"
-  $process = Start-Process -FilePath "$filename" -ArgumentList "INSTALLCFG=`"$workd\jreinstall.cfg`"" -Wait -PassThru
+  Write-Verbose "Install JRE from $filename"
+
+  Invoke-Installer -FilePath "$filename" -ArgumentList "INSTALLCFG=`"$workd\jreinstall.cfg`""
+# Unfortunately JRE gives back the console and continues installing in background for some time
+# Waiting for background jreinstall process
 
 # Set a timeout for jreinstall to finish
   $timeout = (Get-Date).AddMinutes($jreinstall_timeout)
 
-# Wait for the installation to finish
-  Write-Host 'Waiting for jreinstall.exe to finish...'
-  while ((Get-Process jreinstall -ErrorAction SilentlyContinue) -and ((Get-Date) -le $timeout))
-  {
-    Start-Sleep -Seconds 5
+# Wait for the installation to finish and print a '.' each 10 seconds of waiting
+  Write-Host -NoNewLine 'Waiting for jreinstall.exe to finish'
+  while ((Get-Process jreinstall -ErrorAction SilentlyContinue) -and ((Get-Date) -le $timeout)) {
+    Write-Host -NoNewLine '.'
+    Start-Sleep -Seconds 10
   }
+  Write-Host '' # just a new line to fix console output after NoNewLine
 
 # Try to force-kill jreinstall to prevent further install conflicts
-  if (Get-Process jreinstall -ErrorAction SilentlyContinue)
-  {
+  if (Get-Process jreinstall -ErrorAction SilentlyContinue) {
     Stop-Process -Name jreinstall -Force
   }
 
 # Remove the installer
   rm -Force $workd\jre*
-
-  Print-ExecInfo $process
 }
 
-function Install-VcRedist([string]$vcrsource = $script:vcrsource, [string]$vcrsource_filter = $script:vcrsource_filter)
-{
+function Install-VcRedist([string]$vcrsource = $script:vcrsource, [string]$vcrsource_filter = $script:vcrsource_filter) {
   $filename = Download-File -uri $vcrsource -msdl_filter $vcrsource_filter
 
   Write-Verbose "Installing VcRedist from $filename"
-  $process = Start-Process -FilePath "$filename" -ArgumentList "/Q /log $workd\vcredist.log" -Wait -PassThru
   
-  Start-Sleep -Seconds 5
-  Print-ExecInfo $process
+  Invoke-Installer -FilePath "$filename" -ArgumentList "/Q /log $workd\vcredist.log"
 }
 
-function Install-Gateway([string]$filename = $script:path, [string]$workd = $script:workd)
-{
+function Install-Gateway([string]$filename = $script:path, [string]$workd = $script:workd) {
 # uninstall any existing gateway
   UnInstall-Gateway
-
-  Write-Host 'Start Gateway installation'
   
   Write-Verbose "Installing Gateway from $filename"
-  
-  $process = Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $filename /quiet /passive /L*v $workd\ir.log" -Wait -PassThru
-  Start-Sleep -Seconds 5
 
-  Write-Host "Gateway installer finished with exitCode: $($process.exitcode)"
-
-  Print-ExecInfo $process
+  Invoke-Installer -FilePath 'msiexec.exe' -ArgumentList "/i $filename /quiet /passive /L*v $workd\ir.log"
 }
 
-function Register-Gateway([string] $key)
-{
+function Register-Gateway([string] $key) {
   Write-Host "Start to register gateway with key: $key"
+
   $executable = "C:\Program Files\Microsoft Integration Runtime\5.0\Shared\dmgcmd.exe"
   
-  if (test-path $executable)
-  {
+  if (test-path $executable) {
     $p1 = Start-Process -FilePath $executable -ArgumentList @("-EnableRemoteAccess", "8060") -Wait -PassThru
     $p2 = Start-Process -FilePath $executable -ArgumentList @("-RegisterNewNode", "$key", "$env:COMPUTERNAME") -Wait -PassThru
   }
 
-  switch($p1.exitCode + $p2.exitCode)
-  {
-    0
-    {
+  switch($p1.exitCode + $p2.exitCode) {
+    0 {
       Write-Host "Registration with the gateway was successul"
     }
-    $null
-    {
+    $null {
       Write-Host "Register gateway impossible, executable not found"
     }
-    default
-    {
+    default {
       Write-Host "Gateway registration exit codes $($p1.exitCode)] [$($p2.exitCode)]"
     }
   }
 }
 
-function Check-WhetherGatewayInstalled([string]$name)
-{
-  $installedSoftwares = Get-ChildItem "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
-    foreach ($installedSoftware in $installedSoftwares)
-    {
-      $displayName = $installedSoftware.GetValue("DisplayName")
-        if($DisplayName -eq "$name Preview" -or  $DisplayName -eq "$name")
-        {
-          return $true
-        }
-    }
-
-  return $false
-}
-
-function UnInstall-Gateway()
-{
+function UnInstall-Gateway() {
+# searching WMI for any integration runtime and performing uninstall
   $product = Get-WmiObject -Class Win32_Product -Filter "Name LIKE 'Microsoft Integration Runtime%'" -ComputerName . -ErrorAction SilentlyContinue
-  if ($product)
-  {
+  if ($product) {
     Write-Verbose "Found Microsoft Integration Runtime`n$($product | Format-Table Name, Version, IdentifyingNumber)"
     [void]$product.Uninstall()
     Write-Host "Microsoft Integration Runtime $($product.Version) has been uninstalled."
-  }
-  else
-  {
+  } else {
     Write-Host "Microsoft Integration Runtime not found."
   }
 }
 
-function Print-FileInfo($filename)
-{
-  # get file versionInfo and 
-  $info = Get-Item $filename |
-    Select-Object -Expand VersionInfo |
-    Select-Object FileName, FileVersion, ProductVersion |
-    ForEach-Object { $_.psobject.properties } |
-    ForEach-Object { "$($_.Name)=$($_.Value)" }
-  
-  #output a single line for better visibility
-  Write-Host "$($info -join '; ')"
+function Invoke-Installer([Parameter(Mandatory=$true)][string]$FilePath, [string]$ArgumentList) {
+# wraps around start-process to get some useful info from the executed process
+  $process = Start-Process -FilePath $FilePath -ArgumentList $ArgumentList -PassThru -Wait
+
+  Print-ObjectInfo -psObject $process -propertyNames @('StartTime','ExitTime','ExitCode')
 }
 
-function Print-ExecInfo($processinfo)
-{
-  # prepare process object
-  $process = $processinfo |
-    Select-Object exitCode, startTime, exitTime |
-    ForEach-Object { $_.psobject.properties } |
-    ForEach-Object { "$($_.Name)=$($_.Value)" }
+function Print-ObjectInfo([object]$psObject, [string[]]$propertyNames) {
+# extracts properties from a powershell object and outputs them to the console
+  $info = $psObject |
+    ForEach-Object {
+      $_.psobject.properties |
+        Where-Object { $propertyNames -contains $_.Name } |
+          ForEach-Object { "$($_.Name)=$($_.Value)" }
+    }
 
-  #output a single line for better visibility
-  Write-Host "$($process -join '; ')"
+  Write-Host "$($info -join '; ')"
 }
 
 # Script starting parameters (including default)
 $CommandName = $PSCmdlet.MyInvocation.InvocationName # get the script own name
 $ParameterList = (Get-Command -Name $CommandName).Parameters # Get the list of parameters for the script
-$inputParams = foreach ($Parameter in $ParameterList) # extract parameter=value
-{
+$inputParams = ForEach ($Parameter in $ParameterList) {
+# extract parameter=value and put into inputParams for the report below
   Get-Variable -Name $Parameter.Values.Name -ErrorAction SilentlyContinue | Select-object Name,Value
 }
-$runreport = @(
+$startupreport = @(
   "ParameterSetName:  $($PSCmdlet.ParameterSetName)",
   "ParameterCount:    $($PSBoundParameters.Count)",
   "PsBoundParameters: $($PsBoundParameters.getEnumerator())",
   "Script parameters (and their defaults):$($inputParams|Out-String)"
 )
-Write-Verbose "`n$($runreport -join "`n")"
+Write-Verbose "`n$($startupreport -join "`n")"
 
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-      [Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
+      [Security.Principal.WindowsBuiltInRole] "Administrator")) {
   Write-Error -Category PermissionDenied -Message "You do not have Administrator rights to run this script!`n Please re-run this script as an Administrator!"
   Break
 }
 
 # Working directory tests/creation
-If (-NOT (Test-Path -Path $workd -PathType Container))
-{
+If (-NOT (Test-Path -Path $workd -PathType Container)) {
   New-Item -Path $workd -ItemType Directory | Out-Null
-    if ($?)
-    {
+    if ($?) {
       Write-Host "Created directory: $workd"
-    }
-    else
-    {
+    } else {
       Write-Host "Failed to create directory: $workd"
     }
 }
 
 Install-VcRedist
 Install-JRE
-switch ($PSCmdlet.ParameterSetName)
-{
-  'Latest'
-  {
+switch ($PSCmdlet.ParameterSetName) {
+  'Latest' {
     # if path does not exist or not set falling back to IR remote install / latest version
     $path = Download-File -uri $irsource -msdl_filter $irsource_filter
   }
-  'Compatibility'
-  {
+  'Compatibility' {
     # Compatibility mode uses positional parameter w/ a different name because of PS param handling limitations
     $authKey = $authKey1
   }
